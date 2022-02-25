b26c328cb3800d2e7c9886fc4fc3168939ea5cbb
akashwebs <aksah.shil227@gmail.com>
2022-02-25 11:25:26 +0600

    completed see datils

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
-const searchFood = () => {
+const searchFood = async() => {
 
     const srachFood = document.getElementById('serach-value');
     const srachName = srachFood.value;
 
     srachFood.value = ''
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${srachName}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayResult(data.meals)
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayResult(data.meals))
 }
 
 const displayResult = meals => {
+    const seeDatils = document.getElementById('food-details')
+    seeDatils.innerHTML = ''
+
     const serachResult = document.getElementById('serach-result');
+    serachResult.innerHTML = ''
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
+        const mealId = meal.idMeal;
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card">
+        <div onclick='detailsFood(${mealId})' class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -28,4 +33,37 @@ const displayResult = meals => {
         div.className = 'col'
         serachResult.appendChild(div)
     })
+}
+
+const detailsFood = async meal => {
+    const serachResult = document.getElementById('serach-result');
+    serachResult.innerHTML = ''
+
+    const mealId = meal;
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+
+    const res = await fetch(url);
+    const data = await res.json();
+    seeDatils(data);
+
+}
+
+const seeDatils = data => {
+
+    const seeDatils = document.getElementById('food-details')
+    seeDatils.innerHTML = ''
+
+
+    const meal = data.meals[0];
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">See Video</a>
+            </div>
+            `
+    seeDatils.appendChild(div);
 }
